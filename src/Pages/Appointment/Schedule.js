282a9a8fc0631d95539329c8ca23da412e7a75f2
muzahid604 <604muzahid@gmail.com>
2022-05-12 20:57:33 +0600

    schedule fake data loaded

--- src/Pages/Appointment/Schedule.js
+++ src/Pages/Appointment/Schedule.js
@@ -5,7 +5,8 @@ const Schedule = ({ schedule }) => {
         <div class="card lg:w-80 bg-base-100 shadow-xl">
             <div class="card-body  text-center">
                 <h2 class="text-2xl text-primary">{schedule.name}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <p className='text-red-500 text-xl'>{schedule.slots.length < 1 ? 'Please try another date' : ''}</p>
+                <p>{schedule.slots.length} {schedule.slots.length > 1 ? 'SPACES' : 'SPACE'} AVAILABLE</p>
                 <div class="card-actions justify-center">
                     <button class="btn btn-primary uppercase">Appointment</button>
                 </div>
