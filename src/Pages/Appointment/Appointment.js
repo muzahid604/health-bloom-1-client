7480a2b82b59e86248fc0fb636b2a7d7085c447f
muzahid604 <604muzahid@gmail.com>
2022-05-12 16:10:59 +0600

    date picker added

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Footer from '../Common/Footer/Footer';
 
 const Appointment = () => {
     return (
         <div>
-            <h1>this is appointment</h1>
+            <div className="hero lg:px-12 min-h-screen bg-[url('https://i.ibb.co/fp8fK7p/bg.png')]">
+                <div className="hero-content flex-col lg:flex-row-reverse ">
+                    <img src="https://i.ibb.co/g98Hy7t/chair.png" className=" lg:max-w-sm lg:rounded-lg shadow-2xl" alt='' />
+                    <div>
+                        <h1 className="text-2xl lg:text-5xl font-bold">Your New Smile Starts Here</h1>
+                        <p className="py-6 lg:text-xl">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-primary uppercase bg-gradient-to-r from-secondary to-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
