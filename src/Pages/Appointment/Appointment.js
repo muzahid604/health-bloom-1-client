9424c112aab0da63ed5ab91d5b8405be8deac52c
muzahid604 <604muzahid@gmail.com>
2022-05-12 16:25:50 +0600

    useing day picker

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 import Footer from '../Common/Footer/Footer';
-
+import { format } from 'date-fns';
 const Appointment = () => {
+    const [selected, setSelected] = useState(new Date());
+
+    let footer = <p>Please pick a day.</p>;
+    if (selected) {
+        footer = <p>You picked {format(selected, 'PP')}.</p>;
+    }
     return (
         <div>
             <div className="hero lg:px-12 min-h-screen bg-[url('https://i.ibb.co/fp8fK7p/bg.png')]">
                 <div className="hero-content flex-col lg:flex-row-reverse ">
                     <img src="https://i.ibb.co/g98Hy7t/chair.png" className=" lg:max-w-sm lg:rounded-lg shadow-2xl" alt='' />
                     <div>
-                        <h1 className="text-2xl lg:text-5xl font-bold">Your New Smile Starts Here</h1>
-                        <p className="py-6 lg:text-xl">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary uppercase bg-gradient-to-r from-secondary to-primary">Get Started</button>
+                        <DayPicker
+                            mode="single"
+                            selected={selected}
+                            onSelect={setSelected}
+                            footer={footer}
+                        />
                     </div>
                 </div>
             </div>
